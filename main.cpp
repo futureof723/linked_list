d058ed4aef8bafd78e27803562ef4cd0e2e28d0f
futur <futureof723@gmail.com>
2025-02-10 10:16:00 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <utility>
+
+struct Link {
+    std::string value;
+    Link* prev;
+    Link* next;
+
+    explicit Link(std::string  value, Link* p = nullptr, Link* s = nullptr)
+        : value(std::move(value)), prev(p), next(s) {}
+};
+
+int main() {
+    // Create the first node ("Thor")
+    const auto thor = new Link("Thor");
+
+    // Create the second node ("Odin"), linking it with "Thor"
+    const auto odin = new Link("Odin", nullptr, thor);
+    thor->prev = odin;
+
+    // Create the third node ("Freia"), linking it with "Odin"
+    const auto freia = new Link("Freia", nullptr, odin);
+    odin->prev = freia;
+
+    // Now the list is: "Thor" <-> "Odin" <-> "Freia"
+
+    // traversing the list forwards
+    auto current = freia;
+    while (current != nullptr) {
+        std::cout << current->value << std::endl;
+        current = current->next;
+    }
+
+    std::cout << "==============================================" << std::endl;
+
+    // traversing the list backwards
+    auto backwards = thor;
+    while (backwards != nullptr) {
+        std::cout << backwards->value << std::endl;
+        backwards = backwards->prev;
+    }
+
+    // Delete all nodes to prevent memory leak
+    delete freia;
+    delete odin;
+    delete thor;
+
+}
