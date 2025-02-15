f4b4df0f00ef6e3f2d5b42a8ff3d3417bfa3b66e
futur <futureof723@gmail.com>
2025-02-15 07:12:02 -0500

    Fixed the ordering and pointing of the gods it goes thor -> odin -> freia. Refactored code. Added traversing backwords function.

--- main.cpp
+++ main.cpp
@@ -20,35 +20,59 @@ void display_forwards(const Link* head) {
     }
 }
 
+void display_backwards(Link* head) {
+    if (head == nullptr) {
+        return;
+    }
+    while (head->next != nullptr) {
+        head = head->next;
+    }
+    while (head != nullptr) {
+        std::cout << head->value << std::endl;
+        head = head->prev;
+    }
+}
 int main() {
-    // Create the first node ("Thor")
     const auto thor = new Link("Thor");
+    const auto odin = new Link("Odin", nullptr, nullptr);
+    const auto freia = new Link("Freia", nullptr, nullptr);
 
-    // Create the second node ("Odin"), linking it with "Thor"
-    const auto odin = new Link("Odin", nullptr, thor);
-    thor->prev = odin;
+    // Now linking the gods together: nullptr <-thor -> <- odin -> <-freia -> nullptr
+    thor->next = odin;
+    thor->prev = nullptr;
+    odin->next = freia;
+    odin->prev = thor;
+    freia->next = nullptr;
+    freia->prev = odin;
 
-    // Create the third node ("Freia"), linking it with "Odin"
-    const auto freia = new Link("Freia", nullptr, odin);
-    odin->prev = freia;
-
-    // Now the list is: "Thor" <-> "Odin" <-> "Freia"
-
+    std::cout << "==============================================" << std::endl;
+    std::cout << "Traversing forwards:" << std::endl;
     // traversing the list forwards
-    auto current = freia;
+    auto current = thor;
     while (current != nullptr) {
         std::cout << current->value << std::endl;
         current = current->next;
     }
 
     std::cout << "==============================================" << std::endl;
+    std::cout << "Traversing backwards:" << std::endl;
 
     // traversing the list backwards
-    auto backwards = thor;
+    auto backwards = freia;
     while (backwards != nullptr) {
         std::cout << backwards->value << std::endl;
         backwards = backwards->prev;
     }
+
+    std::cout << "==================================================" << std::endl;
+    std::cout << "Using function to traverse forwards:" << std::endl;
+    display_forwards(thor);
+
+    std::cout << "==================================================" << std::endl;
+    std::cout << "Using function to traverse backwards:" << std::endl;
+    display_backwards(thor);
+
+
 
     // Delete all nodes to prevent memory leak
     delete freia;
